1d357451a927417c483cb3e1512115256fa9b7a7
unknown <harjeej5@gmail.com>
2020-03-13 21:53:19 -0600

    Server and client code added

--- a2/q9/Client.py
+++ a2/q9/Client.py
@@ -1,28 +1,122 @@
 #!/usr/bin/env python3
 
 import socket
+import os
+import random
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
 
-HOSTNAME = '127.0.0.1'
-PORT = 65432
+HOSTNAME = '127.0.4.18'
+PORT = 31802
 
 
 def get_user_input():
-    user = input('Enter Username: ')
-    pw = input('Enter Password: ')
+    user = input('Username:')
+    print(user)
+    pw = input('Password:')
+    print(pw)
+    print("Client: I = '%s'" % user)
 
     return user, pw
 
 
-def setup_server_connection(user, pword):
-    init_connection = True
+def calculate_key_with_hash(salt, password, n_prime, primitive_root):
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(bytearray(salt))
+    digest.update(bytearray(password, 'utf-8'))
+    hashed_val = digest.finalize()
+    hashed_int = int.from_bytes(hashed_val, 'big')
+
+    return int(pow(primitive_root, hashed_int, n_prime))
+
+
+def setup_server_connection(user, password):
+    salt = os.urandom(16)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOSTNAME, PORT))
-        s.sendall(b'Hello World')
-        data = s.recv(1024)
-        print('Received', repr(data))
-        data = s.recv(1024)
-        print('Received', repr(data))
+        n_prime = s.recv(1024)
+        n_prime = int(n_prime, 16)
+
+        primitive_root = s.recv(1024)
+        primitive_root = int(primitive_root, 16)
+
+        v = calculate_key_with_hash(salt, password, n_prime, primitive_root)
+        hex_v = bytes(v.to_bytes(64, byteorder='big')).hex()
+
+        print("Client: Sending 'r' <%s>" % bytes('r', 'utf-8').hex())
+        s.sendall(bytes('r', 'utf-8'))
+
+        username_size = bytes(len(user).to_bytes(4, byteorder='big')).hex()
+        print('Client: Sending |I| <%s>' % username_size)
+        s.sendall(bytes(username_size, 'utf-8'))
+
+        user_hex = bytes(user, 'utf-8').hex()
+        print('Client: Sending I <%s>' % user_hex)
+        s.sendall(bytes(user, 'utf-8'))
+
+        salt_bytes = bytes(salt)
+        print('Client: s = <%s>' % salt_bytes.hex())
+        print('Client: Sending s <%s>' % salt_bytes.hex())
+        s.sendall(salt_bytes)
+
+        print('Client: v = %d' % v)
+        print('Client: Sending v <%s>' % bytes(hex_v, 'utf-8'))
+        s.sendall(bytes(hex_v, 'utf-8'))
+
+        print('Client: Registration successful')
+
+        print('Client: N = %d' % n_prime)
+        print('Client: g = %d' % primitive_root)
+
+    return n_prime, primitive_root
 
 
-username, password = get_user_input()
-setup_server_connection(username, password)
+def compute_big_a(n_prime, primitive_root):
+    a = random.randint(0, n_prime)
+    big_a = int(pow(primitive_root, a, n_prime))
+
+    a_hex = a.to_bytes(64, byteorder='big')
+    print('Client: a = %s' % a_hex.hex())
+
+    big_a_hex = big_a.to_bytes(64, byteorder='big')
+    print('Client: A = %s' % big_a_hex.hex())
+
+    return big_a
+
+
+def negotiation_with_server(big_a, username):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOSTNAME, PORT))
+
+        print("Client: Sending 'p' <%s>" % bytes('p', 'utf-8').hex())
+        s.sendall(bytes('p', 'utf-8'))
+
+        username_size = bytes(len(username).to_bytes(4, byteorder='big')).hex()
+        print('Client: Sending |I| <%s>' % username_size)
+        s.sendall(bytes(username_size, 'utf-8'))
+
+        user_hex = bytes(username, 'utf-8').hex()
+        print('Client: Sending I <%s>' % user_hex)
+        s.sendall(bytes(username, 'utf-8'))
+
+        big_a = bytes(big_a.to_bytes(64, byteorder='big')).hex()
+        print('Client: Sending A <%s>' % big_a)
+        s.sendall(bytes(big_a, 'utf-8'))
+
+        salt = s.recv(16).hex()
+        print('Client: s = <%s>' % salt)
+
+        big_b_raw = s.recv(64)
+        big_b = int(big_b_raw, 16)
+        print('Client: B = %d' % big_b)
+
+
+def init():
+    username, password = get_user_input()
+    n_prime, primitive_root = setup_server_connection(username, password)
+
+    big_a = compute_big_a(n_prime, primitive_root)
+    negotiation_with_server(big_a, username)
+
+
+init()
--- a2/q9/Server.py
+++ a2/q9/Server.py
@@ -2,32 +2,117 @@
 import os
 from sympy import isprime
 import socket
+import random
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
 
-HOSTNAME = '127.0.0.1'
-PORT = 65432
+HOSTNAME = '127.0.0.1'  # '127.0.4.18'
+PORT = 65432  # 31802
+
+
+def calculate_big_b(n_prime, hex_prime, primitive_root, hex_root, v):
+    b = int(random.randint(0, n_prime))
+
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(bytes(hex_prime, 'utf-8'))
+    digest.update(bytes(hex_root, 'utf-8'))
+    hashed_val = digest.finalize()
+
+    k = int.from_bytes(hashed_val, 'big')
+    summation = int(pow(primitive_root, b, n_prime)) + k*v
+
+    big_b = summation % n_prime
+
+    b_hex = bytes(b.to_bytes(64, byteorder='big')).hex()
+    print('Server: b = %s' % b_hex)
+
+    k_hex = bytes(k.to_bytes(32, byteorder='big')).hex()
+    print('Server: k = %s' % k_hex)
+
+    big_b_hex = bytes(big_b.to_bytes(64, byteorder='big')).hex()
+    print('Server: B = %s' % big_b_hex)
+
+    return big_b, b
+
+
+def compute_u(big_a, big_b, n_prime):
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(bytes(big_a.to_bytes(64, byteorder='big')))
+    digest.update(bytes(big_b.to_bytes(64, byteorder='big')))
+    hashed_val = digest.finalize()
+
+    hash_num = int.from_bytes(hashed_val, 'big')
+    return hash_num % n_prime
+
+
+def compute_server_key(big_a, v, u, b, n_prime):
+    A_v = big_a * v
+    u_b = u * b
+    return int(pow(A_v, u_b, n_prime))
 
 
 def setup_client_connection(n_prime, primitive_root):
-    init_connection = True
-    print('Entered method')
-    hex_prime = hex(n_prime)
-    hex_root = hex(primitive_root)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+        print('Server is up')
+        print('Server: N = %d' % n_prime)
+        print('Server: g = %d' % primitive_root)
+        hex_prime = bytes(n_prime.to_bytes(64, byteorder='big')).hex()
+        hex_root = bytes(primitive_root.to_bytes(64, byteorder='big')).hex()
         soc.bind((HOSTNAME, PORT))
         soc.listen()
         conn, addr = soc.accept()
         with conn:
-            print('Connected by', addr)
-            if init_connection:
-                init_connection = False
-                conn.sendall(bytes(hex_prime, 'utf-8'))
-                conn.sendall(bytes(hex_root, 'utf-8'))
-            while True:
+            print('Server: Sending N <%s>' % hex_prime)
+            print('Server: Sending g <%s>' % hex_root)
+            conn.sendall(bytes(hex_prime, 'utf-8'))
+            conn.sendall(bytes(hex_root, 'utf-8'))
+
+            r = conn.recv(1).decode('utf-8')
+            i_size = int(conn.recv(8), 16)
+
+            i = conn.recv(i_size)
+            print("Server: I = '%s'" % i.decode('utf-8'))
+
+            salt = conn.recv(16)
+            print("Server: s = <%s>" % salt.hex())
+
+            v_raw = conn.recv(16)
+            v = int(v_raw, 16)
+            hex_v = bytes(v.to_bytes(64, byteorder='big')).hex()
+            print('Server: v = %s' % hex_v)
+
+            print('Server: Registration successful')
+
+        conn, addr = soc.accept()
+        with conn:
+            big_b, b = calculate_big_b(n_prime, hex_prime, primitive_root, hex_root, v)
+            data = conn.recv(1)
+            while not data.decode('utf-8') == 'p':
                 data = conn.recv(1024)
-                print(data)
-                if not data:
-                    break
-                conn.sendall(data)
+
+            username_size = int(conn.recv(8), 16)
+            username = conn.recv(username_size).decode('utf-8')
+            big_a = int(conn.recv(64), 16)
+
+            print("Server: I = '%s'" % username)
+            print("Server: A = %d" % big_a)
+
+            print('Server: Sending s <%s>' % salt.hex())
+            conn.sendall(salt)
+
+            print('Server: Sending B <%s>' % big_b)
+            big_b_bytes = bytes(big_b.to_bytes(64, byteorder='big')).hex()
+            conn.sendall(bytes(big_b_bytes, 'utf-8'))
+
+            u = compute_u(big_a, big_b, n_prime)
+            hex_u = bytes(u.to_bytes(32, byteorder='big')).hex()
+            print('Server: u = %s' % hex_u)
+
+            server_key = compute_server_key(big_a, v, u, b, n_prime)
+
+            hex_server_key = bytes(server_key.to_bytes(64, byteorder='big')).hex()
+            print('Server: k_server = %s' % hex_server_key)
+            conn.sendall(bytes(hex_server_key, 'utf-8'))
 
 
 def verify_candidate(candidate, sophie_prime, prime):
@@ -66,7 +151,6 @@ def initial_values():
             n_prime = 2 * test_prime + 1
 
             if isprime(n_prime):
-                binary_n = bin(n_prime)
                 primitive_root = find_primitive_root(test_prime, n_prime)
 
                 if primitive_root == -1:
